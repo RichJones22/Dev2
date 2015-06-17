e6fe69eb9287d00e93514c2a939592e3b21e6d97
Rich Jones <jones_rich@yahoo.com>
2015-06-17 10:59:21 -0500

    got a background image on my main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
 
             <div class="masthead clearfix">
                 <div class="inner">
-                    <h3 class="masthead-brand">Premise Software Solutions</h3>
+                    <h3 class="masthead-brand">Premise Software</h3>
                     <nav>
                         <ul class="nav masthead-nav">
                             <li class="active"><a href="#">Home</a></li>
@@ -39,8 +39,8 @@
             </div>
 
             <div class="inner cover">
-                <h1 class="cover-heading">Cover your page.</h1>
-                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                <h1 class="cover-heading">Custom Open Source Development</h1>
+                {{--<p class="lead">Custom Open Source Development.</p>--}}
                 {{--<p class="lead">--}}
                     {{--<a href="#" class="btn btn-lg btn-default">Learn more</a>--}}
                 {{--</p>--}}
@@ -48,7 +48,8 @@
 
             <div class="mastfoot">
                 <div class="inner">
-                    <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                    {{--<p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>--}}
+                    <p>Premise Software Solutions, Inc.</p>
                 </div>
             </div>
 
